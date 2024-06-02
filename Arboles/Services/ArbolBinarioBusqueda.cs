9cdc967db60c1f99481bc163750cf3078c23b5e8
Sofia <asofimp@gmail.com>
2024-06-02 11:51:00 -0600

    cambio sorry

--- Arboles/Services/ArbolBinarioBusqueda.cs
+++ Arboles/Services/ArbolBinarioBusqueda.cs
@@ -5,10 +5,19 @@ namespace Arboles.Services
     public class ArbolBinarioBusqueda
     {
         public Nodo? NodoRaiz { get; set; }
+        public int ? TotalNodos { get; set; }
 
         public ArbolBinarioBusqueda()
         {
             NodoRaiz = null;
+        }
+
+        bool ArbolVacio()=> NodoRaiz == null;
+
+        public string InsertarNodo (string nodo)
+        {
+            if(ArbolVacio()) { }
+            return "Hola mundo";
         }
     }
 }
