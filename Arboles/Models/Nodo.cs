7614d1ac08b7d3a102225b591f737861e13a5e17
GerardoSamayoa <gerardo.samayoa@noduu.com>
2024-06-01 21:40:29 -0600

    nodo

--- Arboles/Models/Nodo.cs
+++ Arboles/Models/Nodo.cs
@@ -2,5 +2,23 @@
 {
     public class Nodo
     {
+        public Nodo? RamaIzquierda { get; set; }
+
+        public int? Informacion { get; set; }
+
+        public Nodo? RamaDerecha { get; set; }
+        public Nodo()
+        {
+            RamaIzquierda = null;
+            Informacion = 0;
+            RamaDerecha = null;
+        }
+
+        public Nodo(int? informacion)
+        {
+            RamaIzquierda = null;
+            Informacion = informacion;
+            RamaDerecha = null;
+        }
     }
 }
